68713e4411a15e902de8243988c02122d60fa891
DRypa <dr.drypa.k@mail.ru>
2017-04-07 15:40:53 +0300

    timer

---
+++ src/ArangoDbTests/StopwatchTimer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ArangoDbTests
+{
+    public class StopwatchTimer : IDisposable
+    {
+        private readonly Action<int> _callback;
+        private readonly Stopwatch _stopwatch;
+        public StopwatchTimer(Action<int> callback)
+        {
+            _callback = callback;
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+            _callback((int)_stopwatch.Elapsed.TotalMilliseconds);
+        }
+    }
+}
